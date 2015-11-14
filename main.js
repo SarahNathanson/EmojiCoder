7f5d6f3a53d538846bfe24d15019344fedf5506d
SarahNathanson <sarah.j.nathanson@gmail.com>
2015-11-14 15:08:37 -0500

    implemented stack

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+var stack=new Array();//create the stack
+compileLine("👇parm");
+compileLine("👆");
+function compileLine(script){
+	
+	var command=script.slice(0,2);//emoji representation is 2 chars long
+	var parameter=script.slice(2);//parameter is everything after first emoji
+	console.log("Command: "+command);
+	console.log("Parameter: "+parameter);
 
-function compile(script){
+	//tests
+	stack.push(parameter);
+	console.log(stack.pop());
 
+	if(command == "👇"){
+		console.log("push");
+		stack.push(parameter);
+	}else if(command=="👆"){
+		console.log("pop");
+		var x=stack.pop();
+		console.log(x);
+	}
 }
 
 function getFormData(){
